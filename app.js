52b11a80d946d7684a8ffad698977940c2210dce
Neilson86 <134339003+Neilson86@users.noreply.github.com>
2024-11-07 12:11:25 -0300

    Primeira Atualização

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Page Loaded");
+});
+
+const PRIMARY_COLOR = '#23A6F0';
+const TEXT_COLOR = '#252B42';
+const SECOND_TEXT_COLOR = '#737373';
+const LIGHT_TEXT_COLOR = '#FFFFFF';
